4d676e0c5d5004857b79049045a250585709cc8b
ruslan <ruslanone57@gmail.com>
2025-12-06 02:02:51 +0700

    add: threading rm(useless)

--- src/utils/base_util.py
+++ src/utils/base_util.py
@@ -1,8 +1,7 @@
-from concurrent.futures import ThreadPoolExecutor
 
 class BaseUtil:
     def __init__(self, path: str) -> None:
-        self._thread_pool = ThreadPoolExecutor(max_workers=10)
+        
         self._path = path
     
     
--- src/utils/rm.py
+++ src/utils/rm.py
@@ -1,12 +1,14 @@
 import os
+import multiprocessing as mp
+from concurrent.futures import ThreadPoolExecutor
 from utils.base_util import BaseUtil
 
 class RMutil(BaseUtil):
     def __init__(self, path: str) -> None:
         super().__init__(path)
+        self._thread_pool = ThreadPoolExecutor(max_workers=700)
         
-    def _remove_default(self, path):
-        
+    def _remove_default(self, path):  
         dirs = next(os.walk(path))[1]
         files = next(os.walk(path))[2]
 
@@ -22,8 +24,40 @@ class RMutil(BaseUtil):
             self._remove_default(dir_path)
             os.rmdir(dir_path)
     
-    def run(self):
-        self._remove_default(self._path)
-            
 
+    #threading is useless for remove
+    def _remove_with_threads(self, path):
+        dirs = next(os.walk(path))[1]
+        files = next(os.walk(path))[2]
+
+        if len(dirs) == 0:
+            for file in files:
+                file_path = os.path.join(path, file)
+                os.remove(file_path)
+            return
+        
+        futures = []
+        for dir in dirs:
+            dir_path = os.path.join(path, dir)
+            futures.append((self._thread_pool.submit(self._remove_with_threads, dir_path), dir_path))
+
+        for file in files:
+            file_path = os.path.join(path, file)
+            os.remove(file_path)
+
+        for future in futures:
+            res, arg = future
+            res.result()
+            os.rmdir(arg)
+
+    def _remove_with_multiprocessing(self, path):
+        pass
+           
+        
+    def run(self):
+        
+        
+               
+        
+        
         
